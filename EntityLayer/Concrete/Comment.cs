512a7a583ee77dd5d00bd1531ef83ba27f04a412
Yavuz <yavuz.mert@outlook.com>
2024-09-02 22:26:09 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentID { get; set; }
+
+
+        public string CommentUserName { get; set; } // yorumu yapan kişinin
+
+
+        public string CommentTitle { get; set; }
+
+
+        public string CommentContent { get; set; }
+
+
+        public DateTime CommentDate { get; set; }
+
+
+        public bool CommentStatus { get; set; }
+    }
+}
